d03dbb45d4e5e577e0ddf9a254ff9fba0707dc36
Kevin McAleer <kevinmcaleer@gmail.com>
2022-04-24 16:01:59 +0100

    Update eyes.py

--- eyes.py
+++ eyes.py
@@ -5,6 +5,7 @@
 import plasma
 from plasma import plasma2040
 from time import sleep
+from random import choice
 
 NUM_LEDS = 2
 FPS = 60
@@ -14,18 +15,27 @@ WAIT = 0.01
 led_strip = plasma.WS2812(NUM_LEDS, 0, 0, plasma2040.DAT, rgbw=True, color_order=plasma.COLOR_ORDER_GRB)
 led_strip.start(FPS)
 
+RED = 1.0
+BLUE = 0.6
+GREEN = 0.3
+PINK = 0.9
+
+colours = [RED,BLUE,GREEN,PINK]
+
 def red_glow():
+    
+    eyes = choice(colours)
     for i in range(1,100,1):
         value = i/100
-        led_strip.set_hsv(0,1,1,value)
-        led_strip.set_hsv(1,1,1,value)
+        led_strip.set_hsv(0,eyes,1,value)
+        led_strip.set_hsv(1,eyes,1,value)
         sleep(WAIT)
 
     for i in range(100,1,-1):
         value = i/100
-        led_strip.set_hsv(0,1,1,value)
-        led_strip.set_hsv(1,1,1,value)
+        led_strip.set_hsv(0,eyes,1,value)
+        led_strip.set_hsv(1,eyes,1,value)
         sleep(WAIT)
 
 while True:
-    red_flow()
+    red_glow()
